a20eedfc14033aa969bac8cd4a4c5ea8dd072a25
Norbert Jakubiec <norbertjakubiec93@gmail.com>
2022-10-24 23:37:44 +0200

    commit 1

---
+++ src/main/java/com/cokroktoupadek/beer_ap/BeerApApplication.java
@@ -0,0 +1,13 @@
+package com.cokroktoupadek.beer_ap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerApApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerApApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cokroktoupadek/beer_ap/BeerApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cokroktoupadek.beer_ap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerApApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
